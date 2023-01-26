73615d1676f8244f02d26f4a493427dfa9c6a6ba
Owen Grimaldi <ogrimald@student10.cse.nd.edu>
2023-01-26 10:24:45 -0500

    added info on turns, tile adding

--- boardSetup.py
+++ boardSetup.py
@@ -1,6 +1,13 @@
 '''
 Board setup for Conway's, namely creating the data structure, its border, and conditions.
 '''
+'''
+Conway's Rules per Turn:
+    1.) Any live cell with two or three live neighbours survives.
+    2.) Any dead cell with three live neighbours becomes a live cell.
+    3.) All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+'''
 
 #!/usr/bin/env python3
 
@@ -30,5 +37,24 @@ class Board:
         print("+", end="")
         for x in range(self.boardSize): print("--", end="")
         print("-+") #extra dash added to account for spacing tiles out in tile rows
+
+    def addTile(self, posRow, posCol):
+        #what if tile is already alive? ping user in higher level func
+        #sets tile to alive on board position given, row and col
+        self.tiles[posRow][posCol] = 'x'
+
+
+    def executeTurn(self):
+    #A call performs the algorithm that occurs each turn of Conway's
+    #Evaluates current board for neighbors, creates new board, replaces old
+
+    newBoard = self.tiles
+
+        for currRow in range(self.boardSize):
+            for currCol in range(self.boardSize):
+                
+
+                
+                
 
 
